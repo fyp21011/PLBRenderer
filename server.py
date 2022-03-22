0fe25b1b893a87fdc6558b8c3d6046af4f668aa8
EE-LiuYunhao <liuyunhao1578@gmail.com>
2022-03-22 04:16:26 +0000

    fix the animation

--- server.py
+++ server.py
@@ -1,21 +1,30 @@
 import asyncio
 import os
 import tempfile
+import sys
 from typing import Callable, Dict, Type
 
 import bpy
 import mathutils
 
+cwd = os.getcwd()
+sys.path.append(cwd)
+
 from protocol import *
 
 
 def _set_item_pose(item, pose):
+    """ #TODO
+    """
     assert len(pose) == 7, \
         f"expecting a pose vector of length 7 (3 location dims, 4 rotation dims), got {len(pose)}"
     item.location = pose[:3]
+    item.rotation_mode = 'QUATERNION'
     item.rotation_quaternion = mathutils.Quaternion(pose[3:])
 
 def add_rigid_body_mesh_message_handler(message: AddRigidBodyMeshMessage):
+    """ #TODO
+    """
     filename = message.mesh_name
     suffix = filename.split('.')
     if suffix:
@@ -40,30 +49,54 @@ def add_rigid_body_mesh_message_handler(message: AddRigidBodyMeshMessage):
 
 
 def add_rigid_body_primitive_message_handler(message: AddRigidBodyPrimitiveMessage):
-    message.create_primitive_in_blender()
+    """ #TODO
+    """
+    #message.create_primitive_in_blender()
+    eval(message.primitive_type)(**message.params)
     item = bpy.context.active_object
     item.name = message.primitive_name
 
 def set_particles_message_handler(message: SetParticlesMessage):
+    """ #TODO
+    """
     vertices_np = message.particles.reshape((-1, 3))
     num_vertices = vertices_np.shape[0]
     vertices = [
         vertices_np[i, :]
         for i in range(num_vertices)
     ]
-    if message.obj_name not in bpy.context.objects:
+    if message.obj_name not in bpy.data.objects:
+        # no such object, create one meshes object
         meshes = bpy.data.meshes.new(message.obj_name + "_point_cloud")
         object = bpy.data.objects.new(message.obj_name, meshes)
+        bpy.context.collection.objects.link(object)
     else:
-        object = bpy.context.object[message.obj_name]
+        # retrieve the existing meshes and prepare to edit it
+        object = bpy.data.objects[message.obj_name]
+        bpy.context.view_layer.objects.active = object
         meshes = object.data
-    
+        bpy.ops.object.mode_set(mode = "OBJECT")
+        bpy.ops.object.shape_key_add(from_mix = False)
+        meshes.clear_geometry()
+
     meshes.from_pydata(vertices, [], [])
-    meshes.update()
+    meshes.update()   
+    # animation
+    shapekey = object.shape_key_add(f"key {message.frame_idx}", False)
+    shapekey.value = 1
+    object.keyframe_insert(data_path = "value", frame = message.frame_idx)
+    if message.prev_frame_idx != None:
+        shapekey.value = 0
+        object.keyframe_insert(data_path = "value", frame = message.prev_frame_idx)
+        meshes.shape_keys.key_blocks[f"key {message.prev_frame_idx}"].value = 0
+        object.keyframe_insert(data_path = "value", frame = message.frame_idx)
+
 
 def update_rigid_body_mesh_pose_message(message: UpdateRigidBodyPoseMessage):
-    if message.name in bpy.context.objects:
-        obj = bpy.context.objects[message.name]
+    """ #TODO
+    """
+    if message.name in bpy.data.objects:
+        obj = bpy.data.objects[message.name]
         _set_item_pose(obj, message.pose_vec)
         obj.keyframe_insert(data_path = 'location', frame = message.frame_idx)
         obj.keyframe_insert(data_path = 'rotation_quaternion', frame = message.frame_idx)
@@ -79,6 +112,8 @@ MSG_CALLBACK_TABLE: Dict[Type, Callable[[BaseMessage], None]] = {
 }
 
 def callback_entrance(message: BaseMessage) -> None:
+    """ #TODO
+    """
     message_type = type(message)
     if message_type in MSG_CALLBACK_TABLE:
         MSG_CALLBACK_TABLE[message_type](message)
@@ -90,23 +125,63 @@ def server_main():
     asyncio.run(server.run_server())
 
 def test_main():
+    test_rotation = [
+        [1.0000, 0.0000, 0.0000, 0.0000],
+        [0.6603, 0.0000, 0.0000, -0.7510],
+        [0.1280, 0.0000, 0.0000, 0.9918],
+        [0.8293, 0.0000, 0.0000, 0.5588],
+        [0.9673, -0.0000, -0.0000, -0.2538],
+        [0.4481, -0.0000, -0.0000, -0.8940],
+        [0.3755, 0.0000, 0.0000, 0.9268],
+        [0.9440, 0.0000, 0.0000, 0.3300],
+        [0.8712, -0.0000, -0.0000, -0.4910],
+        [0.2065, -0.0000, -0.0000, -0.9785],
+    ]
+    
+    
     mock_message = AddRigidBodyPrimitiveMessage(
         "cube_a", 
         "bpy.ops.mesh.primitive_cube_add",
         size = 1.0,
         location = [0.0, 0.1, 0.0],
-        rotation = [0.0, 0.6, 0.0, 0.8],
+        rotation = [0.0, 0.0, 0.6],
         scale = [0.2, 0.2, 1.0]
     )
     callback_entrance(mock_message)
 
+    rotation_cnt = 0
+
     for frame_idx in range(0, 100, 10):
         mock_message = UpdateRigidBodyPoseMessage(
             'cube_a',
-            [
-                0.0, 0.0, 0.1 + 0.01 * frame_idx,
-                0.0, 0.6, 0.0, 0.8
-            ],
+            [0.0, 0.0, 0.1 + 0.01 * frame_idx] + test_rotation[rotation_cnt],
             frame_idx
         )
         callback_entrance(mock_message)
+        rotation_cnt += 1
+        
+        
+def test_set_particles():
+    import numpy as np
+    for i in range(1, 100, 10):
+        message = SetParticlesMessage(
+            np.array([
+                1.0, 1.0, 1.0,
+                0.0, 0.0, 0.0,
+                1.0, 0.0, 0.0, 
+                1.0, 0.0, 1.0,
+                1.0, 1.0, 0.0,
+                0.0, 0.0, 0.0, 
+                0.0, 0.0, 1.0,
+                0.0, 1.0, 0.0,
+                0.0, 1.0, 1.0,
+                0.2, 0.4, 0.7,
+                0.6, 0.2, 0.5
+            ]) + i / 10,
+            'test_points',
+            0
+        )
+        callback_entrance(message)
+        
+# test_set_particles()
+test_main()
